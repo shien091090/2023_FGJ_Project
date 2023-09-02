9434c59f2851ac80549790435942a8fdbbd3b8ee
NK.Shien_Home <shien091091@gmail.com>
2023-09-02 15:47:09 +0800

    1.角色跳躍表現  2.設定跳躍力道(500)和重力(3)

--- Assets/Scripts/Character/CharacterView.cs
+++ Assets/Scripts/Character/CharacterView.cs
@@ -4,8 +4,20 @@ public class CharacterView : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
     [SerializeField] private float speed;
-    
+
+    private Rigidbody2D rigidbody;
     private CharacterMoveModel characterMoveModel;
+
+    private Rigidbody2D GetRigidbody
+    {
+        get
+        {
+            if (rigidbody == null)
+                rigidbody = GetComponent<Rigidbody2D>();
+
+            return rigidbody;
+        }
+    }
 
     private void Start()
     {
@@ -24,6 +36,14 @@ public class CharacterView : MonoBehaviour
     {
         characterMoveModel.OnHorizontalMove -= OnHorizontalMove;
         characterMoveModel.OnHorizontalMove += OnHorizontalMove;
+
+        characterMoveModel.OnJump -= OnJump;
+        characterMoveModel.OnJump += OnJump;
+    }
+
+    private void OnJump(float jumpForce)
+    {
+        GetRigidbody.AddForce(new Vector2(0, jumpForce));
     }
 
     private void OnHorizontalMove(float moveValue)
