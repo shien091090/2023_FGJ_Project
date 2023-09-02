de99a6b1fd682c98b781fedf93de36872866c23d
NK.Shien_Home <shien091091@gmail.com>
2023-09-02 20:22:00 +0800

    1.清除範圍TileMap流程  2.Add Test Case : 有設定上下清除範圍

--- Assets/Scripts/Tests/Editor/TileRemoverTest.cs
+++ Assets/Scripts/Tests/Editor/TileRemoverTest.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 public class TileRemoverTest
 {
     private Action<Vector3[]> removeTileEvent;
+    private TileRemoverModel tileRemoverModel;
 
     [SetUp]
     public void Setup()
@@ -17,13 +18,37 @@ public class TileRemoverTest
     //清除範圍為0時, 不會有清除事件
     public void clear_range_is_0()
     {
-        TileRemoverModel tileRemoverModel = new TileRemoverModel(0, 0);
-        tileRemoverModel.OnRemoveTiles += removeTileEvent;
+        tileRemoverModel = CreateModel(0, 0);
 
-        tileRemoverModel.UpdateRemoveTile();
+        tileRemoverModel.UpdateRemoveTile(Vector3.Zero);
 
         removeTileEvent.DidNotReceive().Invoke(Arg.Any<Vector3[]>());
     }
 
+    [Test]
     //有設定上下清除範圍
+    public void have_clear_range_setting()
+    {
+        tileRemoverModel = CreateModel(2, 4);
+
+        tileRemoverModel.UpdateRemoveTile(new Vector3(2, 1, 0));
+
+        removeTileEvent.Received(1).Invoke(Arg.Is<Vector3[]>(x =>
+            x.Length == 7 &&
+            x[0] == new Vector3(2, 1, 0) &&
+            x[1] == new Vector3(2, 3, 0) &&
+            x[2] == new Vector3(2, 2, 0) &&
+            x[3] == new Vector3(2, 0, 0) &&
+            x[4] == new Vector3(2, -1, 0) &&
+            x[5] == new Vector3(2, -2, 0) &&
+            x[6] == new Vector3(2, -3, 0)));
+    }
+
+    private TileRemoverModel CreateModel(int upRange, int downRange)
+    {
+        tileRemoverModel = new TileRemoverModel(upRange, downRange);
+        tileRemoverModel.OnRemoveTiles += removeTileEvent;
+
+        return tileRemoverModel;
+    }
 }
--- Assets/Scripts/TileRemover/TileRemoverModel.cs
+++ Assets/Scripts/TileRemover/TileRemoverModel.cs
@@ -1,15 +1,41 @@
 using System;
-using System.Numerics;
+using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 public class TileRemoverModel
 {
+    private readonly int upRange;
+    private readonly int downRange;
+
     public event Action<Vector3[]> OnRemoveTiles;
 
     public TileRemoverModel(int upRange, int downRange)
     {
+        this.upRange = upRange;
+        this.downRange = downRange;
     }
 
-    public void UpdateRemoveTile()
+    public void UpdateRemoveTile(Vector3 centerPos)
     {
+        if (upRange == 0 && downRange == 0)
+            return;
+
+        Vector3[] removePosArray = new Vector3 [upRange + downRange + 1];
+        removePosArray[0] = centerPos;
+
+        int currentIndex = 1;
+        for (int i = upRange; i > 0; i--)
+        {
+            removePosArray[currentIndex] = new Vector3(centerPos.X, centerPos.Y + i, centerPos.Z);
+            currentIndex += 1;
+        }
+
+        for (int i = 0; i < downRange; i++)
+        {
+            removePosArray[currentIndex] = new Vector3(centerPos.X, centerPos.Y - 1 - i, centerPos.Z);
+            currentIndex += 1;
+        }
+
+        OnRemoveTiles?.Invoke(removePosArray);
     }
 }
