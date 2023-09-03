9dee5ad769d3f69dbc8ab6fdf7243ef0614e1fe8
NK.Shien_Home <shien091091@gmail.com>
2023-09-04 00:21:02 +0800

    1.使用秒數型道具時每次更新會發事件通知&時間結束後通知道具使用完畢  2.Add Test Case : 使用秒數限制型道具, 使用後過指定時間消失

--- Assets/Scripts/Item/ItemModel.cs
+++ Assets/Scripts/Item/ItemModel.cs
@@ -5,15 +5,31 @@ public class ItemModel
     private readonly ItemUseType itemUseType;
     private readonly float useLimit;
     private int currentUseTimes;
+    private float currentTimer;
 
     public event Action OnItemUseComplete;
     public event Action<int> OnRefreshCurrentUseTimes;
+    public event Action<float> OnRefreshCurrentTimer;
 
     public ItemModel(ItemUseType itemUseType, float useLimit)
     {
         this.itemUseType = itemUseType;
         this.useLimit = useLimit;
         currentUseTimes = (int)useLimit;
+        currentTimer = useLimit;
+    }
+
+    public void UpdateTimer(float deltaTime)
+    {
+        if (itemUseType == ItemUseType.UseTimes)
+            return;
+
+        currentTimer = Math.Max(currentTimer - deltaTime, 0);
+
+        OnRefreshCurrentTimer?.Invoke(currentTimer);
+
+        if (currentTimer <= 0)
+            OnItemUseComplete?.Invoke();
     }
 
     public void UseItem()
--- Assets/Scripts/Tests/Editor/ItemModelTest.cs
+++ Assets/Scripts/Tests/Editor/ItemModelTest.cs
@@ -6,6 +6,7 @@ public class ItemModelTest
 {
     private Action itemUseCompleteEvent;
     private Action<int> refreshCurrentUseTimesEvent;
+    private Action<float> refreshCurrentTimerEvent;
 
     [Test]
     //使用次數限制型道具, 僅限制1次, 使用完即消失
@@ -18,7 +19,7 @@ public class ItemModelTest
         ShouldReceiveItemUseCompleteEvent(1);
         ShouldReceiveRefreshUseTimesEvent(1, 0);
     }
-    
+
     [Test]
     //使用次數限制型道具, 限制多次, 使用完即消失
     public void use_item_multiple_times()
@@ -26,14 +27,40 @@ public class ItemModelTest
         ItemModel itemModel = CreateModel(ItemUseType.UseTimes, 2);
 
         itemModel.UseItem();
-        
+
         ShouldReceiveItemUseCompleteEvent(0);
         ShouldReceiveRefreshUseTimesEvent(1, 1);
-        
+
         itemModel.UseItem();
 
         ShouldReceiveItemUseCompleteEvent(1);
         ShouldReceiveRefreshUseTimesEvent(1, 0);
+    }
+
+    [Test]
+    //使用秒數限制型道具, 使用後過指定時間消失
+    public void use_item_pass_time()
+    {
+        ItemModel itemModel = CreateModel(ItemUseType.PassTime, 3);
+
+        itemModel.UseItem();
+
+        itemModel.UpdateTimer(1);
+        ShouldReceiveItemUseCompleteEvent(0);
+        ShouldReceiveRefreshTimerEvent(1, 2);
+
+        itemModel.UpdateTimer(1);
+        ShouldReceiveItemUseCompleteEvent(0);
+        ShouldReceiveRefreshTimerEvent(1, 1);
+
+        itemModel.UpdateTimer(2);
+        ShouldReceiveItemUseCompleteEvent(1);
+        ShouldReceiveRefreshTimerEvent(1, 0);
+    }
+
+    private void ShouldReceiveRefreshTimerEvent(int triggerTimes, float expectedTimerValue)
+    {
+        refreshCurrentTimerEvent.Received(triggerTimes).Invoke(expectedTimerValue);
     }
 
     private void ShouldReceiveRefreshUseTimesEvent(int triggerTimes, int expectedCurrentUseTimes)
@@ -55,9 +82,11 @@ public class ItemModelTest
 
         refreshCurrentUseTimesEvent = Substitute.For<Action<int>>();
         itemModel.OnRefreshCurrentUseTimes += refreshCurrentUseTimesEvent;
+        
+        refreshCurrentTimerEvent = Substitute.For<Action<float>>();
+        itemModel.OnRefreshCurrentTimer += refreshCurrentTimerEvent;
 
         return itemModel;
     }
-    //使用秒數限制型道具, 使用後過指定時間消失
     //使用秒數限制型道具, 等待過程中再次使用沒反應
 }
