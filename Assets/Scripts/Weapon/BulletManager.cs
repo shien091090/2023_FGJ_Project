836e27273fb633a676e2289f0117c9d07fe15dce
NK.Shien_Home <shien091091@gmail.com>
2023-09-04 18:57:11 +0800

    1.新增子彈Prefab  2.實作發射子彈時物件生成位置和面向

---
+++ Assets/Scripts/Weapon/BulletManager.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform objHolder;
+    [SerializeField] private Transform character;
+    
+    private List<GameObject> bulletObjectPool;
+
+    private void Start()
+    {
+        bulletObjectPool = new List<GameObject>();
+
+        ItemStateManager.Instance.OnUseItemOneTime -= OnUseItemOneTime;
+        ItemStateManager.Instance.OnUseItemOneTime += OnUseItemOneTime;
+    }
+
+    private GameObject GetBulletObject()
+    {
+        List<GameObject> hidingObjs = bulletObjectPool.Where(x => x.activeSelf == false).ToList();
+        if (hidingObjs.Count > 0)
+            return hidingObjs[0];
+        else
+        {
+            GameObject newObj = Instantiate(prefab, objHolder);
+            bulletObjectPool.Add(newObj);
+            return newObj;
+        }
+    }
+
+    private void FaceToTargetPos(Transform transform, Vector3 facePos)
+    {
+        Vector3 dir = facePos - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+    }
+
+    private void OnUseItemOneTime(ItemType itemType)
+    {
+        GameObject bulletObject = GetBulletObject();
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+
+        bulletObject.transform.position = character.position;
+        FaceToTargetPos(bulletObject.transform, pos);
+    }
+}
