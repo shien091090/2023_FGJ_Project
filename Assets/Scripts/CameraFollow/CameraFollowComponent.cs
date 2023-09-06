e3c774d8f6e03cfed764e364639450c053575b2e
NK.Shien_Home <shien091091@gmail.com>
2023-09-06 08:52:35 +0800

    攝影機追蹤位置Y軸指定為追蹤目標的Y軸, 修正角色不自然抖動

--- Assets/Scripts/CameraFollow/CameraFollowComponent.cs
+++ Assets/Scripts/CameraFollow/CameraFollowComponent.cs
@@ -9,6 +9,6 @@ public class CameraFollowComponent : MonoBehaviour
     public void Update()
     {
         Vector2 pos = Vector2.SmoothDamp(transform.position, followTarget.position, ref velocity, smoothTime);
-        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        transform.position = new Vector3(pos.x, followTarget.position.y, transform.position.z);
     }
 }
