205774f5cef025e2378c4d3e1e59dda57527de29
NK.Shien_Home <shien091091@gmail.com>
2024-02-04 16:07:41 +0800

    1.開始倒數計時後每秒觸發事件流程  2.Add Test Case : 開始倒數計時, 每秒觸發一次更新事件

--- Assets/Scripts/Tests/Editor/TimerModelTest.cs
+++ Assets/Scripts/Tests/Editor/TimerModelTest.cs
@@ -1,13 +1,19 @@
+using System;
+using NSubstitute;
 using NUnit.Framework;
 
 public class TimerModelTest
 {
     private TimerModel timerModel;
+    private Action timerUpdateEvent;
 
     [SetUp]
     public void Setup()
     {
         timerModel = new TimerModel();
+
+        timerUpdateEvent = Substitute.For<Action>();
+        timerModel.OnUpdateTimer += timerUpdateEvent;
     }
 
     [Test]
@@ -17,12 +23,41 @@ public class TimerModelTest
         CurrentTimeShouldBe(0);
     }
 
+    [Test]
+    //開始倒數計時, 每秒觸發一次更新事件
+    public void start_timer_trigger_update_event_every_second()
+    {
+        timerModel.StartTimer();
+        timerModel.UpdateTimer(0.3f); //0.3
+        timerModel.UpdateTimer(0.3f); //0.6
+        timerModel.UpdateTimer(0.3f); //0.9
+
+        ShouldTiggerUpdateEvent(0);
+        
+        timerModel.UpdateTimer(0.3f); //1.2
+        timerModel.UpdateTimer(0.3f); //1.5
+        timerModel.UpdateTimer(0.3f); //1.8
+        
+        ShouldTiggerUpdateEvent(1);
+        
+        timerModel.UpdateTimer(0.3f); //2.1
+        
+        ShouldTiggerUpdateEvent(2);
+    }
+
+    private void ShouldTiggerUpdateEvent(int expectedTriggerTimes)
+    {
+        if (expectedTriggerTimes == 0)
+            timerUpdateEvent.DidNotReceive().Invoke();
+        else
+            timerUpdateEvent.Received(expectedTriggerTimes).Invoke();
+    }
+
     private void CurrentTimeShouldBe(int expectedCurrentTime)
     {
         Assert.AreEqual(expectedCurrentTime, timerModel.CurrentTime);
     }
-    
-    //開始倒數計時, 每秒觸發一次更新事件
+
     //驗證倒數計時器格式(時:分:秒)
     //驗證倒數計時器格式(分:秒)
     //驗證倒數計時器格式(秒)
--- Assets/Scripts/Timer/TimerModel.cs
+++ Assets/Scripts/Timer/TimerModel.cs
@@ -1,4 +1,26 @@
+using System;
+
 public class TimerModel
 {
-    public float CurrentTime => 0;
+    private float updateEventTimer;
+    public event Action OnUpdateTimer;
+    public float CurrentTime { get; private set; }
+
+    public void UpdateTimer(float deltaTime)
+    {
+        CurrentTime += deltaTime;
+        updateEventTimer += deltaTime;
+
+        while (updateEventTimer >= 1)
+        {
+            OnUpdateTimer?.Invoke();
+            updateEventTimer -= 1;
+        }
+    }
+
+    public void StartTimer()
+    {
+        CurrentTime = 0;
+        updateEventTimer = 0;
+    }
 }
